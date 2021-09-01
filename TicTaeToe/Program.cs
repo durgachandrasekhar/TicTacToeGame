f9f657557393e7da0ae2cfcd0955888fba8274cd
durgachandrasekhar <durgachandrasekhar3@gmail.com>
2021-09-01 10:31:48 +0530

    TicTacToeGame

---
+++ TicTaeToe/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace TicTacToe
+{
+    public class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Console.WriteLine("Welcome to Tic-Tac-Toe Game");
+            TicTacToeGame tictactoegame = new TicTacToeGame();
+            tictactoegame.InitializingBoard();
+        }
+
+
+    }
+
+}
---
+++ TicTaeToe/TicTacToe.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    class TicTacToeGame
+    {
+            public void InitializingBoard()
+            {
+                char[] board = new char[10];
+
+                for (int i = 0; i < 10; i++)
+                {
+                    board[i] = ' ';
+                }
+
+            }
+        }
+    }
+
